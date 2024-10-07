8834b6a45b0a3f715b84ae68289d08aab1ded11f
Vicky Chen <vickyc@andrew.cmu.edu>
2024-10-07 15:21:45 -0400

    initial commit, added Andrew ID query case in QueryProcessor.tsx and added new test case in QueryProcessor.test.ts and all tests passed

--- __tests__/utils/QueryProcessor.test.ts
+++ __tests__/utils/QueryProcessor.test.ts
@@ -17,4 +17,12 @@ describe("QueryProcessor", () => {
             "writer in the English language and the world's pre-eminent dramatist."
           ));
     });
+
+    test('should return vickyc andrewID', () => {
+        const query = "What is Vicky's Andrew ID";
+        const response: string = QueryProcessor(query);
+        expect(response).toBe((
+            "vickyc"
+        ));
+    })
 });
